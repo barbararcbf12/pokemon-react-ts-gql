0fd593acaf0a018a09b70aff56a02b98bffaffbc
Barbara Ferreira <bbf@charlietango.dk>
2021-09-20 14:53:29 +0200

    setup apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import PokemonsContainer from "./containers/PokemonContainer";
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://beta.pokeapi.co/graphql/v1beta",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
   );
 }
 
