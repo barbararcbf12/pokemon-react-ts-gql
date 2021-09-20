787baa33e907d3e9c0f7495a0a4407b91082cc75
Barbara Ferreira <bbf@charlietango.dk>
2021-09-20 14:52:37 +0200

    create query and generate types

---
+++ src/graphql/get-pokemons.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemon_v2_pokemon(limit: $limit, offset: $offset) {
+      name
+      id
+      height
+      weight
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+      pokemon_v2_pokemonabilities {
+        pokemon_v2_ability {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
