b7fedfd76d35695631f72135c86f53f3e7893471
Barbara Ferreira <bbf@charlietango.dk>
2021-09-30 11:29:28 +0200

    fix styles for ipad & clean up

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -9,13 +9,40 @@ type Props = {
   setSelectedPokemon: Dispatch<any>;
 };
 
+const ObjectStyles = {
+  abilitiesList: {
+    display: "flex",
+    justifyContent: "center",
+    flexFlow: "row wrap",
+    padding: 5,
+  },
+  badge: {
+    margin: 2,
+  },
+  body: { width: "100%" },
+  card: {
+    borderRadius: 10,
+    boxShadow: "5px 5px 15px -3px rgba(0,0,0,0.61)",
+    width: "100%",
+  },
+  cardSubtitle: {
+    padding: 3,
+    display: "flex",
+    justifyContent: "center",
+  },
+  listGroupItem: {
+    padding: "0.5rem 0",
+    justifyContent: "center",
+  },
+};
+
 function Pokemon({ pokemon, openModal, setSelectedPokemon }: Props) {
   const [pokemonImage, setpokemonImage] = useState("");
 
   useEffect(() => {
     let isMounted = true;
     //I am fetching the image from a different endpoint here because from 'https://beta.pokeapi.co/graphql/v1beta'
-    //is return null for 'sprites.other.official-artwork.front_default' for all pokemons
+    //is returning null for 'sprites.other.official-artwork.front_default' for all pokemons.
     //I have found the link below where someone says that there's a bug with the graphql engine
     //Link: https://gitmemory.com/issue/PokeAPI/pokeapi/614/826330809
     async function fetchData(id: number) {
@@ -45,60 +72,33 @@ function Pokemon({ pokemon, openModal, setSelectedPokemon }: Props) {
           openModal();
           setSelectedPokemon(pokemon);
         }}
-        style={{
-          borderRadius: 10,
-          boxShadow: "5px 5px 15px -3px rgba(0,0,0,0.61)",
-          width: "100%",
-        }}
+        style={ObjectStyles.card}
         aria-label={pokemon.name}
       >
         <Card.Img variant="top" src={pokemonImage} alt={pokemon.name} />
-        <Card.Body style={{ width: "100%" }}>
+        <Card.Body style={ObjectStyles.body}>
           <Card.Title>{pokemon.name}</Card.Title>
           <ListGroup variant="flush" as="ul">
-            <ListGroup.Item as="li">
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  margin: 2,
-                }}
-              >
-                <Badge bg="primary">height: {pokemon.height}</Badge>
-                <Badge bg="primary">weight: {pokemon.weight}</Badge>
-              </div>
+            <ListGroup.Item as="li" style={ObjectStyles.listGroupItem}>
+              <Badge bg="primary" style={ObjectStyles.badge}>
+                height: {pokemon.height}
+              </Badge>
+              <Badge bg="primary" style={ObjectStyles.badge}>
+                weight: {pokemon.weight}
+              </Badge>
             </ListGroup.Item>
           </ListGroup>
           <ListGroup variant="flush">
             <ListGroup.Item variant="warning">
-              <Card.Subtitle>
-                <div
-                  style={{
-                    padding: 3,
-                    display: "flex",
-                    justifyContent: "center",
-                  }}
-                >
-                  abilities
-                </div>
+              <Card.Subtitle style={ObjectStyles.cardSubtitle}>
+                abilities
               </Card.Subtitle>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  flexFlow: "row wrap",
-                  padding: 5,
-                }}
-              >
+              <div style={ObjectStyles.abilitiesList}>
                 {pokemon &&
                   pokemon?.pokemon_v2_pokemonabilities?.map((ability: any) => (
                     <div
                       key={ability?.pokemon_v2_ability?.id}
-                      style={{
-                        margin: 1,
-                      }}
+                      style={ObjectStyles.badge}
                     >
                       <Badge bg="secondary">
                         {ability.pokemon_v2_ability?.name}
